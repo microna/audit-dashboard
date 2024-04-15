3c8dfec66f0260cc24d75d01bcaaa4f03ea4b6fd
Kononiuk <hmasteraaa@gmail.com>
2024-04-15 16:26:17 +0300

    removed frontend

--- frontend/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export { Home } from "./Home";
-export { FullPost } from "./FullPost";
-export { AddPost } from "./AddPost";
-export { Registration } from "./Registration";
-export { PieChartWithNeedle } from "./PieChartWithNeedle";
-export { PieChartWithPaddingAngle } from "./PieChartWithPaddingAngle";
-export { TableList } from "./TableList";
-export { Login } from "./Login";
-
